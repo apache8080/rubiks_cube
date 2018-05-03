b34955bc5d7e6b87cf839889ac3e9bb5c572023b
Rishi Desai <desai.rishi1@gmail.com>
2018-05-02 22:28:35 -0500

    abstracted controller keys to a separate singleton

---
+++ src/utils/controller.cpp
@@ -0,0 +1,23 @@
+#include "controller.h"
+
+namespace utils {
+
+unsigned char Controller::ZOOM_IN_KEY = '+';
+unsigned char Controller::ZOOM_OUT_KEY = '-';
+unsigned char Controller::RIGHT_ARROW_KEY = 'l';
+unsigned char Controller::LEFT_ARROW_KEY = 'j';
+unsigned char Controller::UP_ARROW_KEY = 'i';
+unsigned char Controller::DOWN_ARROW_KEY = 'k';
+unsigned char Controller::X_LAYER1_KEY = 'q';
+unsigned char Controller::X_LAYER2_KEY = 'w';
+unsigned char Controller::X_LAYER3_KEY = 'e';
+unsigned char Controller::Y_LAYER1_KEY = 'a';
+unsigned char Controller::Y_LAYER2_KEY = 's';
+unsigned char Controller::Y_LAYER3_KEY = 'd';
+unsigned char Controller::Z_LAYER1_KEY = 'z';
+unsigned char Controller::Z_LAYER2_KEY = 'x';
+unsigned char Controller::Z_LAYER3_KEY = 'c';
+unsigned char Controller::COUNTER_CLOCKWISE_KEY = 'u';
+unsigned char Controller::CLOCKWISE_KEY = 'o';
+
+}
---
+++ src/utils/controller.h
@@ -0,0 +1,29 @@
+#ifndef _CONTROLLER_H_
+#define _CONTROLLER_H_
+
+namespace utils {
+
+class Controller {
+ public:
+  static unsigned char ZOOM_IN_KEY;
+  static unsigned char ZOOM_OUT_KEY;
+  static unsigned char RIGHT_ARROW_KEY;
+  static unsigned char LEFT_ARROW_KEY;
+  static unsigned char UP_ARROW_KEY;
+  static unsigned char DOWN_ARROW_KEY;
+  static unsigned char X_LAYER1_KEY;
+  static unsigned char X_LAYER2_KEY;
+  static unsigned char X_LAYER3_KEY;
+  static unsigned char Y_LAYER1_KEY;
+  static unsigned char Y_LAYER2_KEY;
+  static unsigned char Y_LAYER3_KEY;
+  static unsigned char Z_LAYER1_KEY;
+  static unsigned char Z_LAYER2_KEY;
+  static unsigned char Z_LAYER3_KEY;
+  static unsigned char COUNTER_CLOCKWISE_KEY;
+  static unsigned char CLOCKWISE_KEY;
+};
+
+};  //  namespace utils
+
+#endif
